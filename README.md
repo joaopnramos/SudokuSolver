426bf05496ab25aed431db4f84b1eda0e1611ff1
joaopnramos <jpr1999@gmail.com>
2020-06-04 20:56:55 +0100

    Start of sudoku

---
+++ README.md
@@ -0,0 +1,50 @@
+﻿# Sudoku Solver
+## Backtracking / AC-3 / MRV / Least Containing Value
+
+## I] Requirements
+
+- [Git](https://git-scm.com/downloads) to download the source code
+
+- [python3](https://www.python.org/downloads/) to execute the code
+
+- [pip3](https://pypi.org/project/pip/) to install dependencies
+
+## II] Installation
+
+Clone the project to your local machine:
+```bash
+git clone https://github.com/stressGC/python-backtracking-CSP-sudoku-solver.git
+cd python-backtracking-CSP-sudoku-solver
+```
+
+You will need to install the following modules :
+```bash
+pip3 install argparse itertool 
+```
+
+## III] Execution
+
+The following arguments are available:
+
+- **--help** to get some informations about the script
+```bash
+python solver.py --help
+```
+- **--string** followed by a string representing one or more Sudokus. It will be parsed by the script. Accepted characters : [1,9], "#", "@", "X". A list of 20 sudokus can be found [data/20sudokus.txt](https://raw.githubusercontent.com/stressGC/python-backtracking-CSP-sudoku-solver/master/data/20sudokus.txt) .
+```bash
+python solver.py --string <my_sudoku(s)_as_string>
+```
+- **--level** followed by a level for the default sudoku. 
+Accepted values : ["easy", "medium", "hard"], default : "medium".
+```bash
+python solver.py --level <level>
+```
+
+Note : **--string** and **--level** can't be specified at the same time as they are concurrent.
+
+## IV] Result
+
+All the current computing steps will be printed in console for each one of the sudokus.
+
+![Screenshot of a result](https://github.com/stressGC/python-backtracking-CSP-sudoku-solver/blob/master/img/result.png "Screenshot of a result")
+
---
+++ solver.py
@@ -0,0 +1,77 @@
+import argparse
+import sys
+from sudoku import Sudoku
+from ac3 import AC3
+
+from utils import fetch_sudokus, print_grid
+
+"""
+default sudokus' grid
+"""
+sudokus = dict(
+    easy = "000079065000003002005060093340050106000000000608020059950010600700600000820390000",
+    medium = "102004070000902800009003004000240006000107000400068000200800700007501000080400109",
+    hard = "002008050000040070480072000008000031600080005570000600000960048090020000030800900")
+
+"""
+solve
+solves a sudoku based on its String grid
+"""
+def solve(grid, index, total):
+    
+    print("\nSudoku {}/{} : \n{}".format(index, total, print_grid(grid)))
+
+
+    print("{}/{} : AC3 starting".format(index, total))
+
+
+    # instanciate Sudoku
+    sudoku = Sudoku(grid)
+
+    # launch AC-3 algorithm of it
+    AC3_result = AC3(sudoku)
+
+    # Sudoku has no solution
+    if not AC3_result:
+        print("{}/{} : this sudoku has no solution".format(index, total))
+
+    else:
+        
+        # check if AC-3 algorithm has solve the Sudoku
+        if sudoku.isFinished():
+
+            print("{}/{} : AC3 was enough to solve this sudoku !".format(index,total))
+            print("{}/{} : Result: \n{}".format(index, total, sudoku))
+
+
+
+
+
+if __name__ == "__main__":
+
+    # argument parsing using argparse module
+    # doc @ https://docs.python.org/3/library/argparse.html
+    parser = argparse.ArgumentParser(description='Solve a Sudoku with CSP')
+    parser.add_argument('--string', type=str, help='sudoku as a String, can be multiple sudokus in a row in the same line')
+    parser.add_argument('--level', type=str, default='medium', choices=['easy', 'medium', 'hard'], help='selects default sudoku\'s based on level (default: %(default)s)')
+    args = parser.parse_args()
+    
+    sudoku_grid_as_string = ""
+
+    # if user didnt provide a custom string or its string has not the right format
+    if not args.string:
+
+        # let's use the level's default
+        sudoku_grid_as_string = sudokus[args.level]
+        print("\nUsing default sudoku, level : {}".format(args.level))
+    
+    else:
+
+        sudoku_grid_as_string = args.string
+
+    # fetch sudokus from user input
+    sudoku_queue = fetch_sudokus(sudoku_grid_as_string)
+    
+    # for each sudoku, solve it !
+    for index, sudoku_grid in enumerate(sudoku_queue):
+        solve(sudoku_grid, index + 1, len(sudoku_queue))
